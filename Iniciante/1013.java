19a29677368b6bc2234ed380e8c14f48bc9199f5
Enio Silva <eniojdsilva@hotmail.com>
2021-06-24 15:48:00 -0300

    Create 1013.java

---
+++ Iniciante/1013.java
@@ -0,0 +1,18 @@
+public class 1013 {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner input = new Scanner(System.in);
+
+        int a = input.nextInt();
+        int b = input.nextInt();
+        int c = input.nextInt();
+
+        int maiorAB = (a + b + Math.abs(a - b)) / 2;
+        int maiorABC = (maiorAB + c + Math.abs(maiorAB - c)) / 2;
+
+        System.out.println(maiorABC + " eh o maior");
+        
+        input.close();
+    }
+}
